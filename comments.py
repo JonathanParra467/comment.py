8820ed7e440584145eb5c2c4462aa07c2d7db029
JonathanParra467 <jparra1786@students.mchenry.edu>
2025-09-04 10:55:00 -0500

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,10 @@
+"""
+Add a minimum of 4 comments to the above Python program to make it more understandable.
+"""
+tank_size = 15
+# Define the total distance value
+distance = 600
+# Calculate miles per gallon using math in code
+mpg = distance / tank_size
+# make the result using print
+print("Miles per gallon:", mpg)
